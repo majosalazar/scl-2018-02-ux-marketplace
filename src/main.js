930cec427f1a6445d0c1a431d0def41196eb2064
majosalazar <mariaj.salazarf@gmail.com>
2019-02-15 09:09:22 -0300

    deploy

--- src/main.js
+++ src/main.js
@@ -3,3 +3,9 @@ goInfo = () => {
   products.style.display = 'none';
   infoProduct.style.display = 'block';
 };
+
+inicio = () => {
+  carruselId.style.display = 'block';
+  products.style.display = 'block';
+  infoProduct.style.display = 'none';
+}
